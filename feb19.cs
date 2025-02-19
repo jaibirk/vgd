d5bd363e49d051ddb0dd60484d0751323c0407e3
jaibirk <jaibirk874@stemcollegiate.ca>
2025-02-19 08:48:51 -0700

    Create feb19.cs

---
+++ feb19.cs
@@ -0,0 +1,12 @@
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+//Assignment happens from right to left.
+//You use a single equals character as the assignment operator.
+//To retrieve the value from the variable, you merely use the variable's name.
