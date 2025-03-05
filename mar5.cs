fe9ff5c2ba06460747eb40c3d5bb95fbbbb2a8b2
jaibirk <jaibirk874@stemcollegiate.ca>
2025-03-05 09:05:11 -0700

    Create mar5.cs

---
+++ mar5.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+//you use bool for a true or false value
+//you use string for words or phrases
