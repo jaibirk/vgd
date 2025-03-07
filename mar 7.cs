2bfdcab03605abcab942207fddb8ab5bf076c276
jaibirk <jaibirk874@stemcollegiate.ca>
2025-03-07 10:41:49 -0700

    Create mar 7.cs

---
+++ mar 7.cs
@@ -0,0 +1,20 @@
+// unit 3
+string firstName;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+//unit 4
+string firstName = "Bob";
+Console.WriteLine(firstName);
+//unit 5
+var message = "Hello World!"
+//unit 6
+var message = "Bob";
+Console.WriteLine("Bob");
+var temperature = (34.4m);
+Console.WriteLine(34.4m);
+var number = (3);
+Console.WriteLine(3);
+string phrase = ("Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.");
+Console.Write("Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.");
