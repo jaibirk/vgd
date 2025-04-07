bc3a5a959be8a2e6a8e214962d5d206b76b5f3d7
jaibirk <jaibirk874@stemcollegiate.ca>
2025-04-07 13:47:26 -0600

    Create apr 7.cs

---
+++ apr 7.cs
@@ -0,0 +1,16 @@
+//moduel 5 unit 1 and 2
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+// what i learned: you can do math equations like addition and subraction with + and - symbols
