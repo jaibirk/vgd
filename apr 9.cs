d7ea3af5dfc88a97ee826427d2e639bb9f0e576f
jaibirk <jaibirk874@stemcollegiate.ca>
2025-04-09 09:20:01 -0600

    Create apr 9.cs

---
+++ apr 9.cs
@@ -0,0 +1,9 @@
+//unit 3
+//unit 4
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+// you can use symbols like +,-,*,/ to do math equations
